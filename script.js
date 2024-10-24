84721639efa40adeb1e61906cb818fde975eb7e5
maLu70 <lumasiqueira06@gmail.com>
2024-10-23 21:11:33 -0300

    ajustes

--- script.js
+++ script.js
@@ -52,9 +52,3 @@ function bmf() {
     document.querySelector(".slidBCH").style.display = "none";
     document.querySelector(".slidBMF").style.display = "flex";
 }
-
-function menu() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
